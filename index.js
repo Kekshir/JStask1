23971572f342ed5638f987bc99a4659ffc57af34
Anel <abraranel@gmail.com>
2022-05-30 16:17:34 +0600

    add solution for task 4

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ alert(admin);
 //2
 let ourPlanet = 'Earth';
 let currentUserName = 'Anel';
+
+//3
+//let name = "Ilya";
+//alert( `hello ${1}` ); // hello 1
+//alert( `hello ${"name"}` ); // hello name
+//alert( `hello ${name}` ); // hello Ilya
+
+//4
+let userName = prompt ("Введите Ваше имя?");
+alert(userName);
