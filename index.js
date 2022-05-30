4a9755183e8f433de6c6fee941f18b9276cf550f
Anel <abraranel@gmail.com>
2022-05-30 16:08:19 +0600

    add solution for task 1

---
+++ index.js
@@ -0,0 +1,17 @@
+let admin, name;
+name = 'John';
+admin = name;
+alert(admin);
+
+
+let ourPlanet = 'Earth';
+let currentUserName = 'Anel';
+
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ?
+
+alert( `hello ${"name"}` ); // ?
+
+alert( `hello ${name}` ); // ?
